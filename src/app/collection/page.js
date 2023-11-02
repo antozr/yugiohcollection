cef869d40e541a9a71d8b372ee113758cf2f5e35
anto__dev <antonidwm@gmail.com>
2023-11-02 14:05:38 +0100

    ADD Navbar

---
+++ src/app/collection/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const collectionYGH = () => {
+  return (
+    <div>collectionYGH</div>
+  )
+}
+
+export default collectionYGH
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,23 @@
-
-import './globals.scss'
-
+import Navbar from "./components/NavBar";
+import "./globals.scss";
 
 export const metadata = {
-  title: 'Yu Gi Oh Collection set',
-  description: 'This is an application for view, organize your collection of Yu Gi Oh card. With most different aspect and data over your card.',
-}
+  title: "Yu Gi Oh Collection set",
+  description:
+    "This is an application for view, organize your collection of Yu Gi Oh card. With most different aspect and data over your card.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className="body">{children}</body>
+      <head>
+        <meta charSet="utf8" />
+        <meta name="author" content="antozr" />
+      </head>
+      <body className="body">
+        {children}
+        <Navbar />
+      </body>
     </html>
-  )
+  );
 }
